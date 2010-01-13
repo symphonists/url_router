04b4e35854cda97507eb225973cec34fe9b2e9a7
Robert Philp <rphilp@robert-laptop.(none)>
2010-01-13 13:47:13 +0000

    first commit

---
+++ README.txt
@@ -0,0 +1,24 @@
+
+outer
+------------------------------------
+
+Version: 0.1
+Author: Rob Philp (robert.h.philp@gmail.com)
+Build Date: 2010-01-13
+Requirements: Symphony 2.0.7
+
+
+[INSTALLATION]
+
+1. Upload the 'routing' folder in this archive to your Symphony 'extensions' folder.
+
+2. Enable it by selecting the "Rotuing" entry, choose Enable from the with-selected menu, then click Apply.
+
+3. You can now use the Routing plugin.
+
+[NOTES]
+
+[CHANGELOG]
+
+0.1
+- Initial release, brief documentation forthcoming.
---
+++ extension.driver.php
@@ -0,0 +1,64 @@
+<?php
+
+	Class Extension_Router extends Extension{
+	
+		public function about() {
+			return array('name' => 'URL Router',
+						 'version' => '0.1',
+						 'release-date' => '2010-01-12',
+						 'author' => array('name' => 'Robert Philp',
+										   'website' => 'http://robertphilp.com',
+										   'email' => ''),
+							'description'   => 'Allows URL routing for vanity URLs etc'
+				 		);
+		}
+
+		
+		public function getSubscribedDelegates() {
+			return array(
+				array(
+					'page'		=> '/frontend/',
+					'delegate'	=> 'FrontendPrePageResolve',
+					'callback'	=> 'frontendPrePageResolve'
+				)
+			);
+		}
+
+		public function getRoutes() {
+            return $this->_Parent->Configuration->get('routing');
+        }
+
+		public function frontendPrePageResolve($context) {
+			//$page = $context['parent']->Page;
+			/*echo "<pre>";
+			print_r($context);		
+			echo "</pre>";*/
+			/*$rules = array(
+				array(
+					"from" => "/\/vanity\/(.*?)/i",
+					"to" => "/article/clean-up-your-feed-list/$1"
+				)
+			);*/
+			$routes = $this->getRoutes();
+			$rules = $routes['routes'];
+			#var_dump($rules);
+			$url = $context['page'];
+			#echo "URL IS #$url#";
+			#echo "<br />";
+			$matches = array();
+			foreach($rules as $rule) {
+				#echo "RULE IS #{$rule['from']}#";
+				#echo "<br />";
+				if(preg_match($rule['from'], $url, &$matches) == 1) {
+					#echo "got here";
+					$new_url = preg_replace($rule['from'], $rule['to'], $url);
+				}
+			}
+			#echo "NEW URL IS #$new_url#";
+			if($new_url) $context['page'] = $new_url;
+			//if($context['page'] == "/vanity/") $context['page'] = "/article/clean-up-your-feed-list";
+		}
+			
+	}
+
+?>
